3220dfb785c31e7d2c77dbddd9e69e9de7038ceb
eidlek77 <eidlek77@gmail.com>
2023-11-09 21:10:05 +0900

    비밀번호 가림 및 로그인 버튼 튕김 방지 설정

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -27,7 +27,7 @@ class LoginActivity : AppCompatActivity() {
 
         // 회원가입 버튼 이벤트
         binding.btnSignUp.setOnClickListener {
-            val intent: Intent = Intent(this@LoginActivity,SignUpActivity::class.java)
+            val intent: Intent = Intent(this@LoginActivity, SignUpActivity::class.java)
             startActivity(intent)
         }
 
@@ -36,16 +36,22 @@ class LoginActivity : AppCompatActivity() {
             val email = binding.editEmail.text.toString()
             val password = binding.editPassword.text.toString()
 
-            mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this){ task ->
-                    if(task.isSuccessful) {     //로그인 성공시
-                        val intent: Intent = Intent(this@LoginActivity, MainActivity::class.java)
-                        startActivity(intent)
-                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
-                        finish()
-                    } else {                    //로그인 실패시
-                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
-                        Log.d("login", "ERROR ::::: ${task.exception}")
+            if (email.isNullOrBlank() || password.isNullOrBlank()) {
+                // 이메일 또는 비밀번호가 null 또는 빈 문자열인 경우
+                Toast.makeText(this, "이메일 또는 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show()
+            } else {
+                mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {     //로그인 성공시
+                            val intent: Intent =
+                                Intent(this@LoginActivity, MainActivity::class.java)
+                            startActivity(intent)
+                            Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+                            finish()
+                        } else {                    //로그인 실패시
+                            Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                            Log.d("login", "ERROR ::::: ${task.exception}")
+                        }
                     }
             }
         }
